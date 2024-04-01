344996de083d2a7e10f5bd583fc3c631b6bc7d47
sunil9958 <72134489+sunil9958@users.noreply.github.com>
2024-04-01 17:53:26 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,8 +38,8 @@ async def save_group(bot, message):
             await bot.leave_chat(message.chat.id)
             return
         buttons = [[
-                    InlineKeyboardButton('Rᴜʟᴇs', url="https://telegra.ph/How-to-get-movies--series-from-Bot-10-14-2"),
-                    InlineKeyboardButton('Hᴏᴡ Tᴏ Usᴇ Mᴇ', url="https://t.me/+CRxXz6AAWTQ4ZWFl")
+                    InlineKeyboardButton('Rᴜʟᴇs', url="https://graph.org/How-to-get-movies--series-from-Bot-04-01"),
+                    InlineKeyboardButton('Hᴏᴡ Tᴏ Usᴇ Mᴇ', url="https://t.me/+RK-uQG5u-4RhMzE1")
                   ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         d = await message.reply_text(
@@ -63,8 +63,8 @@ async def save_group(bot, message):
                                                  caption=(script.MELCOW_ENG.format(u.mention, message.chat.title)),
                                                  reply_markup=InlineKeyboardMarkup(
                                                                          [[
-                                                                           InlineKeyboardButton('Rᴜʟᴇs', url="https://telegra.ph/How-to-get-movies--series-from-Bot-10-14-2"),
-                                                                           InlineKeyboardButton('Hᴏᴡ Tᴏ Usᴇ Mᴇ', url="https://t.me/+CRxXz6AAWTQ4ZWFl")
+                                                                           InlineKeyboardButton('Rᴜʟᴇs', url="https://graph.org/How-to-get-movies--series-from-Bot-04-01"),
+                                                                           InlineKeyboardButton('Hᴏᴡ Tᴏ Usᴇ Mᴇ', url="https://t.me/+RK-uQG5u-4RhMzE1")
                                                                          ]]
                                                  ),
                                                  parse_mode=enums.ParseMode.HTML
